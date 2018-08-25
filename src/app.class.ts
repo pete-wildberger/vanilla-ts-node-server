498f0b65750d341b70883f8e41aaf053530a0515
Peter Wildberger <pete.wildberger@gmail.com>
2018-08-24 22:30:30 -0500

    add route rm unnecessary deps

--- src/app.class.ts
+++ src/app.class.ts
@@ -3,25 +3,29 @@ import { Server } from 'http';
 import { Router, request, response } from './lib/Router.class';
 
 export class App extends Router {
-	private PUBLICDIR: string = join(process.cwd(), 'public');
-	constructor(server: Server) {
-		super(server);
-		this.routes();
-	}
-	private routes() {
-		// send html
-		this.get('/', (req: request, res: response) => {
-			console.log('base url hit');
-			res.sendFile(join(this.PUBLICDIR, 'index.html'));
-		});
-		// send json
-		this.get('/message', (req: request, res: response) => {
-			console.log('/message get hit');
-			res.json({ message: "LET's Go" });
-		});
-		this.post('/message', (req: request, res: response) => {
-			console.log('/message post hit');
-			res.json({ message: "LET's Go", bounce: req.body, query: req.query });
-		});
-	}
+  private PUBLICDIR: string = join(process.cwd(), 'public');
+  constructor(server: Server) {
+    super(server);
+    this.routes();
+  }
+  private routes() {
+    // send html
+    this.get('/', (req: request, res: response) => {
+      console.log('base url hit');
+      res.sendFile(join(this.PUBLICDIR, 'index.html'));
+    });
+    // send json
+    this.get('/message', (req: request, res: response) => {
+      console.log('/message get hit');
+      res.json({ message: "LET's Go" });
+    });
+    this.post('/message', (req: request, res: response) => {
+      console.log('/message post hit');
+      res.json({ message: "LET's Go", bounce: req.body, query: req.query });
+    });
+    this.put('/message', (req: request, res: response) => {
+      console.log('/message put hit');
+      res.json({ message: "LET's Go", bounce: req.body, query: req.query, params: req.params });
+    });
+  }
 }
